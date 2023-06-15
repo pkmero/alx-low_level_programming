cb5d8250f16ecd1108753c5e92ba372c8c65fd37
pkmero <darkkillramrgamal@gmail.com>
2023-06-15 13:56:40 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *Description  ' first time using puts function'
+ * return : always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+/*return puts result*/
+	return (0);
+}
