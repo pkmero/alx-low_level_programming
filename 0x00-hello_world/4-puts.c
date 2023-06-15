01714ed563e266493e88682fed51533db985cdd3
pkmero <darkkillramrgamal@gmail.com>
2023-06-15 14:00:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- *Description  ' first time using puts function'
- * return : always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
