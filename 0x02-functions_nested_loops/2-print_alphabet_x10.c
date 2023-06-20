13cf52a134a08d69a3fadfddab31ec95e34c4207
pkmero <darkkillramrgamal@gmail.com>
2023-06-20 14:29:20 -0700

    ..

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the aalphabet 10 times
+ *
+ * Return: always 0 ( success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int k;
+
+	for (k = 1; i <= 10; i++)
+	{
+		for (k = 97; k <= 122; k++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
