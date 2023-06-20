c072efa0f8d930a80990cf4cee4fd13784a11f16
pkmero <darkkillramrgamal@gmail.com>
2023-06-20 16:34:23 -0700

    ..

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,10 +3,6 @@
  * Description: print alphabit used to print alphabet in lower case with new line
  * Return: Always 0 (Success)
  */
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
